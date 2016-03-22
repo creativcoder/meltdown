492ec07b294b78fee19242c353db05beb34b302d
Rahul Sharma <rsconceptx@gmail.com>
2016-03-22 23:26:04 +0530

    adding resume check and content length check

--- src/main.rs
+++ src/main.rs
@@ -10,40 +10,45 @@ use std::fs::File;
 use std::io::Write;
 use hyper::Url as DownloadUrl;
 use hyper::header::{Connection, AcceptRanges};
-use hyper::header::{Headers, Range};
+use hyper::header::{ByteRangeSpec, Range, ContentLength};
 use hyper::Client;
 
 #[derive(Debug)]
 struct Downloader {
+    id: usize,
     url: Url,
     start: u64,
     end: u64,
     file_name: String,
-    cursor:u64
+    cursor: u64,
 }
 
 impl Downloader {
-    fn new(url: &str, start: u64, end: u64, file_name: &str,cursor:u64) -> Self {
+    fn new(id: usize, url: &str, start: u64, end: u64, file_name: &str, cursor: u64) -> Self {
         Downloader {
+            id: id,
             url: url.to_owned(),
             start: start,
             end: end,
             file_name: file_name.to_owned(),
-            cursor:cursor
+            cursor: cursor,
         }
     }
     fn download(&self) {
-        let mut headers = Headers::new();
-        headers.set(Range::bytes(0, 100));
         let client = Client::new();
         let mut res = client.get(&self.url)
-                            .header(Range::bytes(self.start, self.end))
+                            .header(if self.end == 0 {
+                                Range::Bytes(vec![ByteRangeSpec::Last(self.end)])
+                            } else {
+                                Range::bytes(self.start, self.end)
+                            })
                             .header(Connection::keep_alive())
                             .send()
                             .unwrap();
         let mut body: Vec<u8> = Vec::new();
         res.read_to_end(&mut body).unwrap();
-        let mut file = DownloadManager::request_file(&self.file_name[..]);
+        // Id incremented file parts
+        let mut file = DownloadManager::request_file(&format!("{}{}", self.file_name, self.id)[..]);
         file.seek(SeekFrom::Start(self.cursor));
         file.write_all(body.as_slice());
     }
@@ -103,20 +108,61 @@ impl DownloadManager {
         self.state = State::Ready;
     }
     fn start(&mut self) {
-        let mut start_range = 0;
-        let mut end_range = self.block_size as u64;
+        let mut content_length: u64 = 0;
+        match self.check_resume() {
+            (true, len) => {
+                let ContentLength(length) = len;
+                content_length = length;
+            }
+            (false, _) => println!("Download does not support resume"),
+        }
+        let mut start_range: u64 = 0;
+        let mut end_range: u64 = self.block_size as u64;
         for i in 0..self.max_connection {
-            println!("Start Range {:?}", start_range);
-            println!("End Range {:?}", end_range);
-            println!("Downloading part {} of {}", i, self.max_connection);
-            self.task_queue
-                .push(Downloader::new(&self.url, start_range, end_range, "./readme.txt",start_range));
-                
-            self.task_queue[i].download();
-            start_range = end_range + 1;
-            end_range = (start_range - 1) * 2;
+            if end_range >= content_length {
+                Downloader::new(99,
+                                &self.url,
+                                content_length,
+                                0,
+                                "./readme.txt",
+                                content_length)
+                    .download();
+            } else {
+                println!("Downloading part {} of {}", i, self.max_connection);
+                self.task_queue
+                    .push(Downloader::new(i,
+                                          &self.url,
+                                          start_range,
+                                          end_range,
+                                          "./readme.txt",
+                                          start_range));
+                self.task_queue[i].download();
+                start_range = end_range + 1;
+                end_range = (start_range - 1) * 2;
+            }
+        }
+        // self.join_them(self.task_queue);
+    }
+
+    fn check_resume(&self) -> (bool, ContentLength) {
+        let client = Client::new();
+        let head_req = client.head(DownloadUrl::parse(&self.url).unwrap());
+        match head_req.send() {
+            Ok(res) => {
+                (res.headers.has::<AcceptRanges>(),
+                 *res.headers.get::<ContentLength>().unwrap())
+            }
+            Err(_) => (false, ContentLength(0)),
         }
     }
+
+    //    fn join_them(&self,task_queue:Vec<Downloader>) {
+    // let file = self.file_name;
+    // for i in task_queue {
+    // i.file_name
+    // }
+    // }
+
     fn request_file(path: &str) -> File {
         let file = OpenOptions::new()
                        .read(true)
@@ -129,16 +175,12 @@ impl DownloadManager {
             panic!("File open error");
         }
     }
-    fn check_resume(&self) -> bool {
-        // TODO Add HEAD request to get Accept-Range Header
-        false
-    }
 }
 
 fn main() {
     let mut manager = DownloadManager::new();
     manager.add_url("https://wordpress.org/plugins/about/readme.txt")
-           .max_connection(8)
+           .max_connection(4)
            .file("readme.txt")
            .finish();
     let _download_thread = thread::spawn(move || {
